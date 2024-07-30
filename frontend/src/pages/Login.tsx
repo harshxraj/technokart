99cec3fce206072609b0c0de6d05fbefd0e36ec3
harshxraj <harshraj0588@gmail.com>
2024-07-30 12:07:02 +0530

    done

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { Field, Input, Label, Button } from "@headlessui/react";
 import clsx from "clsx";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
 import axios from "axios";
 import AuthContext from "../context/AuthContext";
@@ -10,7 +10,7 @@ const Login: React.FC = () => {
   const { setAuth } = useContext(AuthContext);
   const [email, setEmail] = useState<string>("test@gmail.com");
   const [password, setPassword] = useState<string>("123456");
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const handleLogin = async (event: React.FormEvent) => {
     event.preventDefault();
@@ -31,6 +31,7 @@ const Login: React.FC = () => {
         localStorage.setItem("user", userFullname);
         setAuth({ user: userFullname, token });
         <Navigate to="/" />;
+        navigate("/");
         setTimeout(() => {
           // navigate("/");
           <Navigate to="/" />;
